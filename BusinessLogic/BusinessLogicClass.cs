a014a5123a40e721ac170bff06d5fdf3ecf6ca4a
hossamaboouf <hossamaboouf0@gmail.com>
2019-10-17 17:28:42 +0200

    Createtion

---
+++ BusinessLogic/BusinessLogicClass.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic
+{
+    public class InCap
+    {
+        public string ID;
+        public decimal Participation;
+    }
+
+    public class OutCap
+    {
+        public string ID;
+        public int Quantity;
+
+        public override bool Equals(object obj)
+        {
+            var other = obj as OutCap;
+            return (other != null)
+                    && (this.ID == other.ID)
+                    && (this.Quantity == other.Quantity);
+        }
+
+        public override int GetHashCode()
+        {
+            return ID.GetHashCode() ^ Quantity.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return $"ID: {this.ID}, Quantity: {this.Quantity}";
+        }
+    }
+
+    public static class CapOperations
+    {
+        /*
+         * According to requirements we have to use standard mathematical equation,
+         * Which can be applied on any number of fractions and option value related to each fraction.
+         * 
+         * This Equation is:
+         * 
+         * Xn = (Fn * Val) / |(1 - Sum_F)|
+         * 
+         * Where: 
+         *  Xn => Value related to fraction.
+         *  Fn => Fration represent weight of Xn in total value.
+         *  Val => Value that we start by which.
+         *  Sum_F => Summation of all fractions. 
+         *  |(1 - Sum_F)| => Absolute value of (1 - Sum_F).
+         *  
+         * Note:
+         *  According to diviasion  we need to Round it and convert it from double to int.
+         */
+
+        public static List<OutCap> NormalDistribution(int TotalQty, List<InCap> inCaps)
+        {
+            List<OutCap> outCaps = new List<OutCap>();
+            decimal Sum_F = 0;
+
+            foreach (var inCapItem in inCaps)
+                Sum_F += inCapItem.Participation;
+
+            foreach (var inCapItem in inCaps)
+                outCaps.Add(new OutCap() { ID = inCapItem.ID, Quantity = (int)Math.Abs(Math.Round((inCapItem.Participation * TotalQty) / (1 - Sum_F))) });
+
+            return outCaps;
+        }
+    }
+}
---
+++ BusinessLogic/NunitTest.cs
@@ -0,0 +1,119 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace BusinessLogic
+{ 
+    // Represent blue print that test method will use.
+    public class NormalDistributionTestCase
+    {
+        public int TotalQty { set; get; }
+        public List<InCap> InCaps { set; get; }
+        public List<OutCap> Expected { set; get; }
+        public string TestName { set; get; }
+    }
+
+    // Rsponse for get all test case data from test file line by line 
+    // Then performe each line to be in test case form. 
+    public class TestCaseProvider
+    {
+        public static List<NormalDistributionTestCase> TestCases()
+        {
+            List<NormalDistributionTestCase> testCases = new List<NormalDistributionTestCase>();
+            
+            string path = @"E:\TestCases.txt";
+
+            var file = File.ReadAllLines(path).TakeWhile(line => !string.IsNullOrWhiteSpace(line)).ToList();
+
+            //Looping on each line in file and build test case parameter.
+            foreach (var line in file)
+            {
+                //Temp parameter used to store data after perform it.
+                int totalQty;
+                string testCaseName;
+                List<InCap> inCaps = new List<InCap>();
+                List<OutCap> expected = new List<OutCap>();
+
+                // Remove all white space and square brackets.
+                string trim = line.Replace(" ", "").Replace("[", "").Replace("]", "");
+
+                // Get case name and total quantity.
+                var nameAndTotalQty = trim.Split('@')[0].Split(':');
+                testCaseName = nameAndTotalQty[0];
+                totalQty = int.Parse(nameAndTotalQty[1]);
+
+                // Get all inCaps and outCaps as strings.
+                var inCapsAndOutCaps = trim.Split('@')[1].Split('=');
+                var inCapsArray = inCapsAndOutCaps[0].Replace(">,<", ";").Replace("<", "").Replace(">", "").Split(';');
+                var outCapsArray = inCapsAndOutCaps[1].Replace(">,<", ";").Replace("<", "").Replace(">", "").Split(';');
+
+                //Start Build Test Case
+                foreach (var inCapitem in inCapsArray)
+                {
+                    // Perform each inCap and add it to inCaps list
+                    inCaps.Add(new InCap()
+                    {
+                        ID = inCapitem.Split(',')[0],
+                        Participation = Decimal.Divide(decimal.Parse(inCapitem.Split(',')[1].Split('/')[0]), decimal.Parse(inCapitem.Split(',')[1].Split('/')[1]))
+                    });
+                }
+
+                foreach (var outCapitem in outCapsArray)
+                {
+                    // Perform each outCap and add it to outCaps list
+                    expected.Add(new OutCap()
+                    {
+                        ID = outCapitem.Split(',')[0],
+                        Quantity = int.Parse(outCapitem.Split(',')[1])
+                    });
+                }
+                //End Build Test Case.
+
+                //Add test Case to test cases list.
+                testCases.Add(
+                    new NormalDistributionTestCase()
+                    {
+                        TotalQty = totalQty,
+                        InCaps = inCaps,
+                        Expected = expected,
+                        TestName = testCaseName
+                    }
+                );
+            }
+            return testCases;
+        }
+
+        //Static method return test cases one by one after named each one.
+        public static IEnumerable TestCasesRules()
+        {
+            var cases = TestCaseProvider.TestCases();
+            foreach (var caseitem in cases)
+            {
+                yield return new TestCaseData(caseitem).SetName(caseitem.TestName);
+            } 
+        }
+    }
+
+    [TestFixture]
+    class NunitTest
+    {
+
+       
+        [Test, TestCaseSource(typeof(TestCaseProvider), nameof(TestCaseProvider.TestCasesRules))]
+        public void NormalDistributionTest(NormalDistributionTestCase testCase)
+        {
+            /* Get Actual Data */
+            var actual = CapOperations.NormalDistribution(testCase.TotalQty, testCase.InCaps);
+
+            /* Assert */
+            Assert.AreEqual(testCase.Expected, actual);
+        }
+    }
+}
